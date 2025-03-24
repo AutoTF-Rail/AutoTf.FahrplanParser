07145d58da4c8bfe4b2cbbb13d2ad966a37a99d9
chiouya <chiouyazo@gmail.com>
2025-03-24 15:41:39 +0100

    Throw

---
+++ AutoTf.FahrplanParser/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tesseract;
+
+internal static class Program
+{
+	public static void Main(string[] args)
+	{
+		while (true)
+		{
+			Console.WriteLine("AutoTF Fahrplan Parser");
+			Console.Write("File path:");
+			string? path = Console.ReadLine();
+		
+			if (path == null)
+			{
+				Console.WriteLine("Please enter a valid path.");
+				continue;
+			}
+
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("Please enter a valid path.");
+				continue;
+			}
+
+			using TesseractEngine engine = new TesseractEngine("tessdata/", "deu", EngineMode.Default);
+			using Pix? img = Pix.LoadFromFile(path);
+			using Page? page = engine.Process(img);
+
+			string text = page.GetText();
+			Console.WriteLine(text);
+			
+		}
+	}
+}
