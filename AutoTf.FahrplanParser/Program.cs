943b59b6b939bcc411c15c4c6fde2acf9df74113
chiouya <chiouyazo@gmail.com>
2025-03-25 11:14:47 +0100

    Removed loop

--- AutoTf.FahrplanParser/Program.cs
+++ AutoTf.FahrplanParser/Program.cs
@@ -7,37 +7,34 @@ internal static class Program
 	private static Tesseract _engine = new Tesseract("tessdata/", "deu", OcrEngineMode.Default);
 	public static void Main(string[] args)
 	{
-		while (true)
+		Console.WriteLine("AutoTF Fahrplan Parser");
+
+		foreach (string file in Directory.GetFiles("FahrplanData/"))
 		{
-			Console.WriteLine("AutoTF Fahrplan Parser");
+			Console.WriteLine($"Reading {file}");
+			
+			Mat mat = CvInvoke.Imread(file);
 
-			foreach (string file in Directory.GetFiles("FahrplanData/"))
-			{
-				Console.WriteLine($"Reading {file}");
-				
-				Mat mat = CvInvoke.Imread(file);
-
-				Rectangle trainNumRoi = new Rectangle(17, 11, 134, 44);
-				Rectangle planValidityRoi = new Rectangle(348, 11, 259, 44);
-				Rectangle dateRoi = new Rectangle(805, 11, 190, 44);
-				Rectangle timeRoi = new Rectangle(1066, 11, 160, 44);
-				Rectangle nextStopRoi = new Rectangle(1003, 69, 244, 29);
-				Rectangle currSpeedLimitRoi = new Rectangle(124, 750, 76, 36);
-			
-				Console.WriteLine($"Started at {DateTime.Now.ToString("mm:ss.fff")}");
-			
-				Console.WriteLine($"Date: {ExtractText(dateRoi, mat)}");
-				Console.WriteLine($"Time: {ExtractText(timeRoi, mat)}\n");
-			
-				Console.WriteLine($"Train Number: {ExtractText(trainNumRoi, mat)}");
-				Console.WriteLine($"Plan is{(ExtractText(planValidityRoi, mat).Contains("gültig") ? "" : " not")} valid.");
-				Console.WriteLine($"Next stop: {ExtractText(nextStopRoi, mat)}");
-				Console.WriteLine($"Current speed limit: {ExtractText(currSpeedLimitRoi, mat)}");
-			
-				Console.WriteLine($"Finished at {DateTime.Now.ToString("mm:ss.fff")}");
-			
-				mat.Dispose();
-			}
+			Rectangle trainNumRoi = new Rectangle(17, 11, 134, 44);
+			Rectangle planValidityRoi = new Rectangle(348, 11, 259, 44);
+			Rectangle dateRoi = new Rectangle(805, 11, 190, 44);
+			Rectangle timeRoi = new Rectangle(1066, 11, 160, 44);
+			Rectangle nextStopRoi = new Rectangle(1003, 69, 244, 29);
+			Rectangle currSpeedLimitRoi = new Rectangle(124, 750, 76, 36);
+		
+			Console.WriteLine($"Started at {DateTime.Now.ToString("mm:ss.fff")}");
+		
+			Console.WriteLine($"Date: {ExtractText(dateRoi, mat)}");
+			Console.WriteLine($"Time: {ExtractText(timeRoi, mat)}\n");
+		
+			Console.WriteLine($"Train Number: {ExtractText(trainNumRoi, mat)}");
+			Console.WriteLine($"Plan is{(ExtractText(planValidityRoi, mat).Contains("gültig") ? "" : " not")} valid.");
+			Console.WriteLine($"Next stop: {ExtractText(nextStopRoi, mat)}");
+			Console.WriteLine($"Current speed limit: {ExtractText(currSpeedLimitRoi, mat)}");
+		
+			Console.WriteLine($"Finished at {DateTime.Now.ToString("mm:ss.fff")}");
+		
+			mat.Dispose();
 		}
 	}
 
