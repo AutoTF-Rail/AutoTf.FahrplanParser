c5acf0f68d154224147be069da466bf3262bee78
chiouya <chiouyazo@gmail.com>
2025-03-29 12:43:23 +0100

    Optimized width of additional text

--- AutoTf.FahrplanParser/RegionMappings.cs
+++ AutoTf.FahrplanParser/RegionMappings.cs
@@ -68,7 +68,7 @@ public static class RegionMappings
 	
 	public static Rectangle AdditionalText(Rectangle row)
 	{
-		return new Rectangle(row.X + 451, row.Y, 400, 44);
+		return new Rectangle(row.X + 451, row.Y, 375, 44);
 	}
 	
 	public static Rectangle SpeedLimit(Rectangle row)
